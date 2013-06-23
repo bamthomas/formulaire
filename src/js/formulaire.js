efda1b67d27f057d1b5cf13684cf1078b38597d9
Bruno Thomas <bruno@barreverte.fr>
2013-06-23 22:38:24 +0200

    import de json avec filereader et input type=file

--- src/js/formulaire.js
+++ src/js/formulaire.js
@@ -1,13 +1,17 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     function FormViewModel() {
         this.prenom = null;
         this.nom = null;
         this.prevenir_nom_prenom = null;
         this.prevenir_telephone = null;
     }
-
     var formViewModel = new FormViewModel();
+
+    function warning(text) {
+        var warningHtml = '<div class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Attention!</strong><p>' + text + '</p></div>';
+        $('#form').prepend(warningHtml);
+    }
 
     if (Modernizr.localstorage) {
         $('#save').removeClass('hidden');
@@ -15,11 +19,33 @@ $(document).ready(function() {
             formViewModel[prop] = localStorage['formulaire.' + prop];
         }
     } else {
-        var warningHtml = '<div class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Attention!</strong><p>Votre navigateur ne supporte pas l\'enregistrement de données. Cf <a href="http://caniuse.com/namevalue-storage">http://caniuse.com/namevalue-storage</a>. Les données que vous allez saisir ne pourront pas être sauvegardées.</p></div>';
-        $('#form').prepend(warningHtml);
+        warning('Votre navigateur ne supporte pas l\'enregistrement de données. Cf <a href="http://caniuse.com/namevalue-storage">http://caniuse.com/namevalue-storage</a>. Les données que vous allez saisir ne pourront pas être sauvegardées.');
     }
 
-    $('ul.nav li').click(function() {
+    if (typeof(FileReader) === 'function' && Modernizr.draganddrop) {
+        $('#import').removeClass('hidden');
+    } else {
+        warning('Votre navigateur ne supporte pas le glisser-déposer de fichier local. Cf <a href="http://caniuse.com/#feat=filereader">http://caniuse.com/#feat=filereader</a>. Vous ne pourrez pas importer de formulaire préalablement exporté.');
+    }
+
+    $('#file').on('change', function (evt) {
+        var files = evt.target.files;
+        for (var i = 0, f; f = files[i]; i++) {
+            if (!f.type.match('application/json')) {
+                continue;
+            }
+
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                formViewModel = JSON.parse(e.target.result);
+                ko.applyBindings(formViewModel);
+            };
+            reader.readAsText(f);
+        }
+    });
+
+    $('ul.nav li').click(function () {
         var nav_id = $(this).attr('id');
         var content_id = nav_id.replace('-navbar', '');
 
@@ -29,13 +55,13 @@ $(document).ready(function() {
         $(this).addClass('active');
     });
 
-    $('#export').click(function() {
+    $('#export').click(function () {
         uriContent = "data:application/json," + encodeURIComponent(JSON.stringify(formViewModel));
         window.open(uriContent, 'save');
     });
 
-    $('#save').click(function() {
-        for(var prop in formViewModel) {
+    $('#save').click(function () {
+        for (var prop in formViewModel) {
             localStorage['formulaire.' + prop] = formViewModel[prop];
         }
     });
